6a119f7d15652ad2a63004b686d3c7106decceec
Alex Kelley <adkelley@users.noreply.github.com>
2015-06-22 12:21:59 -0700

    user signup/login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: "users#index"
   get "/users/", to: "user#index", as: "users"
   get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show"
+
+  get "/sign_in", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
 end
