dce8d079ded5f6d0fda6ada5983d6ca9697fc8cc
Alex Kelley <adkelley@users.noreply.github.com>
2015-06-23 16:39:11 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root to: "users#index"
-<<<<<<< HEAD
   get "/d3", to: "welcome#index"
-  get "/json", to: "welcome#data"
-=======
-  #get "/d3", to: "welcome#index"
->>>>>>> sidebar
   get "/users/", to: "user#index", as: "users"
   get "sign_up", to: "users#new"
   post "/users", to: "users#create"
@@ -15,9 +10,6 @@ Rails.application.routes.draw do
   post "/sessions", to: "sessions#create"
   get "/sign_out", to: "sessions#signout"
 
-<<<<<<< HEAD
   get "/search/:query", to: "twitter#search"
-=======
   get 'about/show'
->>>>>>> sidebar
 end
