306b846665499ec537010f6327637e711c48305f
Alex Kelley <adkelley@users.noreply.github.com>
2015-06-22 18:06:37 -0700

    logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, uniqueness: true
+  has_many :querys
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
