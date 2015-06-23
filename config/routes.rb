86cb79921caaa6e0f8bed5ae6484e7a7ce37499c
Alex Kelley <adkelley@users.noreply.github.com>
2015-06-23 16:29:09 -0700

    finalized sign_in/sign_up modals

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'about/show'
-
   root to: "users#index"
-  get "/d3", to: "welcome#index"
+  #get "/d3", to: "welcome#index"
   get "/users/", to: "user#index", as: "users"
   get "sign_up", to: "users#new"
   post "/users", to: "users#create"
@@ -11,4 +9,6 @@ Rails.application.routes.draw do
   get "/sign_in", to: "sessions#new"
   post "/sessions", to: "sessions#create"
   get "/sign_out", to: "sessions#signout"
+
+  get 'about/show'
 end
