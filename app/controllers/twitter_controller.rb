c16f2885c0e7db1b8d0130350a9b7680b950f588
Delmer <delmer814@yahoo.com>
2015-07-07 13:26:36 -0700

    suggest refactoring points for messy action

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,40 +1,30 @@
+require "Tapi"
+
 class TwitterController < ApplicationController
-  include SessionsHelper
-  require "./lib/Tapi.rb"
-
+	
+	# fix indentation
+	# add tests
 	def data
-
-          #binding.pry
-          #if current_user
 	    if (params[:query])
-             user = current_user
-             if user
-               query_params = {}
-               query_params[:query_string] = params[:query]
-               query_params[:word_count_min] = params[:min].to_i
-               query_params[:word_count_max] = params[:max].to_i
-               new_query = Query.new(query_params)
-               user.queries << new_query
+	    	
+             if logged_in?
+               current_user.queries.create(query_properties)
              end
+             
 	     t = Tapi.new(params[:query], 1000, params[:min].to_i, params[:max].to_i)
             else
 	      t = Tapi.new("#tech", 500,4,5000)
 	    end
+	    
 	    render :json => t.data
-          # else
-          #   flash[:error] = "Sorry, you must be logged in to search"
-          #   redirect_to :back
-          # end
-	end
 
-	# def search
-	# 	begin
-	# 		t = Tapi.new
-	# 		t.search(params[:query],500)
-	# 		render :json => t.data
-	# 	rescue RateLimitExceeded => e
-	# 		@e = e
-	# 		render :search
-	# 	end
-	# end
+	end
+private 
+
+   def query_properties
+       query = {}
+       query[:query_string] = params[:query]
+       query[:word_count_min] = params[:min].to_i
+       query[:word_count_max] = params[:max].to_i	
+   end
 end
