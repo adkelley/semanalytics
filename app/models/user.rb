39b17b6749b86db739f6e3216814485043ba9aac
Delmer <delmer814@yahoo.com>
2015-07-07 13:08:41 -0700

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  validates :email, uniqueness: true
+  
   has_many :queries
+  
+  has_secure_password
+  # should include more validation coverage
+  # email, presence etc
+  validates :email, uniqueness: true
+  
 
   def self.confirm(params)
-    @user = User.find_by({email: params[:email]})
-    @user.try(:authenticate, params[:password])
+    user = User.find_by({email: params[:email]})
+    user.try(:authenticate, params[:password])
   end
 end
